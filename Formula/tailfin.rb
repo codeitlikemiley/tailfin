ff1c834c43233dfdcad6d85374dd4ff4fdb6c9d1
Uriah Galang <codeitlikemiley@gmail.com>
2026-08-20 09:59:43 +0800

    formula: fill v0.1.1 tarball sha256s (#5)
    Checksums from the GitHub Release SHA256SUMS so brew install verifies.

--- Formula/tailfin.rb
+++ Formula/tailfin.rb
@@ -13,16 +13,16 @@ class Tailfin < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/codeitlikemiley/tailfin/releases/download/v#{version}/tailfin-aarch64-apple-darwin.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "61035dd20f724e24d6b8377a82e1ce4447b0fe96e93f4c9242ef028175dd6825"
     else
       url "https://github.com/codeitlikemiley/tailfin/releases/download/v#{version}/tailfin-x86_64-apple-darwin.tar.gz"
-      sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+      sha256 "7c17ac5b03c5c212053bbfcad3e8c86d72bad7959be9f84afaecb1c08360b41b"
     end
   end
 
   on_linux do
     url "https://github.com/codeitlikemiley/tailfin/releases/download/v#{version}/tailfin-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+    sha256 "53c331ec97fba9eb11d97b6b0a7c61dfc63fd062c4643e89259f019afac88745"
   end
 
   def install
